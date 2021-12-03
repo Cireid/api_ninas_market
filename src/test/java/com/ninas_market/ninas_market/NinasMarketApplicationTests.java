ab503cd35b9babb6af71b33316ce0bbebefc216f
Dieric <gabrieldieric@gmail.com>
2021-12-03 19:05:59 -0300

    Remoção de testes

--- src/test/java/com/ninas_market/ninas_market/NinasMarketApplicationTests.java
+++ src/test/java/com/ninas_market/ninas_market/NinasMarketApplicationTests.java
@@ -1,13 +1,8 @@
 package com.ninas_market.ninas_market;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NinasMarketApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
